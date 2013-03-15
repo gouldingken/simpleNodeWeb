946f7528dd59dbb993ebf0cc4b3a0b67307fbe0e
gouldingken <gouldingken@gmail.com>
2013-03-15 12:13:10 -0400

    testMultiple

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 var app;
-app = http.createServer(function(req, res) {
+app = http.createServer(function (req, res) {
     var postDataStr = "";
 
     req.setEncoding("utf8");
@@ -10,9 +10,10 @@ app = http.createServer(function(req, res) {
     });
     req.addListener("end", function () {
         res.writeHeader(200, {"Content-Type": "application/json"});
-        res.write(JSON.stringify({result:'test env', env:'testVar_' + process.env.TEST_VAR}));
+        res.write(JSON.stringify({result: 'test env', env: 'testVar_' + process.env.TEST_VAR + '_' + process.env.TEST_VAR2}));
         res.end();
     });
 });
 app.listen(80);//, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:80/');
+console.log('VARS: '+ process.env.TEST_VAR + '_' + process.env.TEST_VAR2);
